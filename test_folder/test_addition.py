8ccb4ba965815f8fef1ba0c2ec62eab55e3450f5
Vini7760 <65764828+Vini7760@users.noreply.github.com>
2024-04-24 21:09:29 +0530

    Update test_addition.py

--- test_folder/test_addition.py
+++ test_folder/test_addition.py
@@ -1,5 +1,6 @@
 from main_code.addition import add
 from main_code_2.subtraction import sub
+from main_code_3.main_code_4.multiplication import mul
 
 def test_addition():
     assert add(2, 3) == 5
@@ -7,3 +8,6 @@ def test_addition():
 
 def test_subtaction():
     assert sub(3, 1) == 2
+
+def test_multiplication():
+    assert mul(3, 1) == 3

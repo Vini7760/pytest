e4d62e45aab379e67d41e5dd5400d6b88ed9187e
Vini7760 <65764828+Vini7760@users.noreply.github.com>
2024-04-24 21:05:01 +0530

    Update test_addition.py

--- test_folder/test_addition.py
+++ test_folder/test_addition.py
@@ -1,5 +1,9 @@
 from main_code.addition import add
+from main_code_2.subtraction import sub
 
 def test_addition():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+def test_subtaction():
+    assert sub(3, 1) == 2

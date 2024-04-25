3ddd4cfb8b110d22dd4b0ff7b909232337014a69
Vini7760 <pallakki217.com>
2024-04-25 10:31:58 +0530

    added new files and folders

--- test_folder/test_addition.py
+++ test_folder/test_addition.py
@@ -1,6 +1,7 @@
-from main_code.addition import add
-from main_code_2.subtraction import sub
-from main_code_3.main_code_4.multiplication import mul
+from Energy.Gru.Inc.a4a_pipe_lib import add
+from Energy.Gru.Inc.a4a_pipe_lib import sub
+from Energy.Gru.Inc.a4a_pipe_lib import mul
+from Energy.Gru.a4a_02_ebo_transform_timeseries import add_2
 
 def test_addition():
     assert add(2, 3) == 5
@@ -11,3 +12,6 @@ def test_subtaction():
 
 def test_multiplication():
     assert mul(3, 1) == 3
+
+def test_add_2():
+    assert add_2(3, 1) == 3
